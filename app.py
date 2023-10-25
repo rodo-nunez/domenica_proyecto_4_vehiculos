fadb6905b3fb98f922f799fc0153efdb9fd981e2
Doménica Pincay <domenicapincay_98@outlook.com>
2023-10-24 19:09:11 -0500

    puerto nuevo

--- app.py
+++ app.py
@@ -1,19 +1,5 @@
-import pandas as pd
-import plotly.express as px
 import streamlit as st
-import altair as alt
 
-alt.renderers.enable("mimetype")
-url = 'https://raw.githubusercontent.com/doalpinc98/proyecto_4_vehiculos/main/vehicles_us.csv'
-car_data = pd.read_csv(url) # leer los datos
-hist_button = st.button('Construir histograma') # crear un botón
-        
-if hist_button: # al hacer clic en el botón
-            # escribir un mensaje
-    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
-            
-            # crear un histograma
-    fig = px.histogram(car_data, x="odometer")
-        
-            # mostrar un gráfico Plotly interactivo
-    st.plotly_chart(fig, use_container_width=True)
+st.header('Lanzar una moneda')
+
+st.write('Esta aplicación aún no es funcional. En construcción.')
