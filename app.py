6bffea8f3fc8f106886786ca9c5a14cfabf1982e
Doménica Pincay <domenicapincay_98@outlook.com>
2023-10-26 10:42:06 -0500

    realice cambio a graficos de la app

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
-
+st.header('Evaluación de vehículos de concesionaria')
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
-hist_button = st.button('Construir histograma') # crear un botón
-        
-if hist_button: # al hacer clic en el botón
-            # escribir un mensaje
-    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
-            
-            # crear un histograma
-    fig = px.histogram(car_data, x="odometer")
-        
-            # mostrar un gráfico Plotly interactivo
+
+hist_button2 = st.button('Histograma vehículo vs condición')
+
+if hist_button2:
+    st.write('Compara el año del vehículo con su condición')
+    fig = px.histogram(car_data, x='model_year', color='condition' )
+    st.plotly_chart(fig, use_container_width=True)
+
+scatter_button = st.button('Diagrama de dispersion de precio por tipo de vehículo ')
+if scatter_button:
+    st.write('Compara el precio por tipo de vehículo ')
+    fig = px.scatter(car_data, x='type', y='price')
     st.plotly_chart(fig, use_container_width=True)
