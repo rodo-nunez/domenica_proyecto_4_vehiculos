6f0d2c99cab8a478fd82caa88dcf063864a33de2
Doménica Pincay <domenicapincay_98@outlook.com>
2023-10-26 09:53:15 -0500

    change of config to private

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
+
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
 hist_button = st.button('Construir histograma') # crear un botón
         
